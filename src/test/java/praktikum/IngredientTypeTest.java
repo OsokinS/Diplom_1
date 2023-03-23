ff7268bed2b37d11a1cc7e9488a384eabb8ea2e8
Serg Osokin <serginjo-osokin@yandex.ru>
2023-03-23 10:06:19 +0300

    Diplom_1 #2

--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -1,17 +1,41 @@
 package praktikum;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.Test;
 
-import java.util.EnumSet;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
 
+
+@RunWith(Parameterized.class)
 public class IngredientTypeTest {
 
-    @ParameterizedTest
-    @EnumSource(value = IngredientType.class, names = {"SAUCE", "FILLING"})
-    public void ingredientTypeIncludeTest(IngredientType ingredientType) {
-        assertTrue(EnumSet.allOf(IngredientType.class).contains(ingredientType));
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+
+    public IngredientTypeTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
     }
+
+
+    @Parameterized.Parameters
+    public static Object[][] dataParametersIngredient() {
+        return new Object[][]{
+                {SAUCE, "Название1", 10},
+                {FILLING, "Название2", 5},
+        };
+    }
+
+    @Test
+    public void checkIngredientTypeTest() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(ingredient.getType(), type);
+    }
+
 }
